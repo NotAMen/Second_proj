b4566f7a635a10bc5c2c850a05537eaf38d00bf7
Wtee35 <wtee@rambler.ru>
2017-09-23 22:49:48 +0300

    Second web_proj
    Simple wep_app created using Express and Node.js

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+ 
+var app = express();
+ 
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+ 
+app.use(express.static(__dirname + "/public"));
+ 
+app.post("/register", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+  response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+ 
+app.get("/", function(request, response){
+     
+    response.send("<h1>Home page</h1>");
+});
+ 
+app.listen(3000);
